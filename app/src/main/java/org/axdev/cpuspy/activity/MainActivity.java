94efc5e88d8b6b9682d9e7cce904a1451b51360e
Rob Beane <robbeane@gmail.com>
2015-06-25 18:53:00 -0400

    Simplify adapter used for SlidingTabLayout
    This is in preperation of using the new Design Support
    Library. Currently it has a few bugs, but we should be
    switching to the upcoming release of the library.
    
    Change-Id: Ie1a67b8b188ca38771fe6be487b819f59dbd2c40
    Signed-off-by: Rob Beane <robbeane@gmail.com>

--- app/src/main/java/org/axdev/cpuspy/activity/MainActivity.java
+++ app/src/main/java/org/axdev/cpuspy/activity/MainActivity.java
@@ -9,23 +9,28 @@ package org.axdev.cpuspy.activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
-import android.preference.PreferenceManager;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Spannable;
 import android.text.SpannableString;
 
 import com.crashlytics.android.Crashlytics;
 
-import org.axdev.cpuspy.CpuSpyApp;
-import org.axdev.cpuspy.CpuStateMonitor;
 import org.axdev.cpuspy.R;
-import org.axdev.cpuspy.adapters.ViewPagerAdapter;
+import org.axdev.cpuspy.fragments.InfoFragment;
+import org.axdev.cpuspy.fragments.TimerFragment;
 import org.axdev.cpuspy.utils.ThemeUtils;
 import org.axdev.cpuspy.utils.TypefaceHelper;
 import org.axdev.cpuspy.utils.TypefaceSpan;
 import org.axdev.cpuspy.widget.SlidingTabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -65,14 +70,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupTabs() {
-        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
-        final int numOfTabs = 2;
-        final CharSequence tabTitles[] = getResources().getStringArray(R.array.tab_title);
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), tabTitles, numOfTabs);
-
         // Assigning ViewPager View and setting the adapter
-        ViewPager pager = (ViewPager) findViewById(R.id.pager);
-        pager.setAdapter(adapter);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        if (viewPager != null) {
+            setupViewPager(viewPager);
+        }
 
         // Assiging the Sliding Tab Layout View
         SlidingTabLayout tabs = (SlidingTabLayout) findViewById(R.id.tabs);
@@ -86,8 +88,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Setting the ViewPager For the SlidingTabsLayout
-        tabs.setViewPager(pager);
+        tabs.setViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        adapter.addFragment(new TimerFragment(), getResources().getString(R.string.tab_title_timers));
+        adapter.addFragment(new InfoFragment(), getResources().getString(R.string.tab_title_info));
+        viewPager.setAdapter(adapter);
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
+        }
     }
 
     protected void onResume() {
